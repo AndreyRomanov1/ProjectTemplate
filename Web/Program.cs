afd14d002c057ec1c000e1f4f39ab6f9f3d0e6c3
AndreyRomanov <aromanovekb@gmail.com>
2025-02-12 11:46:01 +0500

    доработка Program в Web-e

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,11 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.ConfigureServices // TODO добавить сервисы в DI
 
 var app = builder.Build();
+
+MigrateIfNeed(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,30 +24,26 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+return;
+
+void MigrateIfNeed(WebApplication webApplication)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    using var scope = webApplication.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
+    {
+        // TODO Добавить DbContext
+        //var context = services.GetRequiredService<DbContext>();
+        //context.Database.Migrate();
+        Console.WriteLine("Миграция: Успешна применена");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Миграция: Ошибка применения миграции: {ex.Message}");
+    }
 }
